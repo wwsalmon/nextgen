206ccc21d1082e560f9057e9ad564cdda8da373e
Samson Zhang <wwsamson@yahoo.com>
2021-06-07 09:57:14 -0700

    add deletion in endpoint generator

--- components/EndpointGenerator.tsx
+++ components/EndpointGenerator.tsx
@@ -22,8 +22,10 @@ export default function EndpointGenerator({nameUppercase, fields}: { nameUpperca
     const [getSingle, setGetSingle] = useState<boolean>(true);
     const [getPagination, setGetPagination] = useState<boolean>(false);
     const [paginationSkip, setPaginationSkip] = useState<number>(10);
+    const [allowDelete, setAllowDelete] = useState<boolean>(false);
     const [postFields, setPostFields] = useState<EndpointFieldObj>({});
     const [getFields, setGetFields] = useState<EndpointFieldObj>({});
+    const [deleteFields, setDeleteFields] = useState<EndpointFieldObj>({});
 
     useEffect(() => {
         const postFieldEntries = fields.map(field => [field.fieldName, {
@@ -36,11 +38,18 @@ export default function EndpointGenerator({nameUppercase, fields}: { nameUpperca
             value: getFields[field.fieldName] ? getFields[field.fieldName].value : "",
         }]);
 
+        const deleteFieldEntries = fields.map(field => [field.fieldName, {
+            type: getFields[field.fieldName] ? getFields[field.fieldName].type : "param",
+            value: getFields[field.fieldName] ? getFields[field.fieldName].value : "",
+        }]);
+
         const newPostFields = Object.fromEntries(postFieldEntries);
         const newGetFields = Object.fromEntries(getFieldEntries);
+        const newDeleteFields = Object.fromEntries(deleteFieldEntries);
 
         setPostFields(newPostFields);
         setGetFields(newGetFields);
+        setDeleteFields(newDeleteFields);
     }, [fields]);
 
     const PostFieldsArr = Object.entries(postFields).map(([fieldName, fieldParam]) => (
@@ -48,6 +57,10 @@ export default function EndpointGenerator({nameUppercase, fields}: { nameUpperca
     ));
 
     const GetFieldsArr = Object.entries(getFields).map(([fieldName, fieldParam]) => (
+        {fieldName: fieldName, fieldType: fieldParam.type, fieldValue: fieldParam.value}
+    ));
+
+    const DeleteFieldsArr = Object.entries(deleteFields).map(([fieldName, fieldParam]) => (
         {fieldName: fieldName, fieldType: fieldParam.type, fieldValue: fieldParam.value}
     ));
 
@@ -81,16 +94,40 @@ export default function EndpointGenerator({nameUppercase, fields}: { nameUpperca
             <FlexChild>
                 <h2>Endpoint params</h2>
                 <h3>Get</h3>
-                <CheckControl value={allowGet} onChange={e => setAllowGet(e.target.checked)} label="Allow?" id="allowGet"/>
+                <CheckControl
+                    value={allowGet}
+                    onChange={e => setAllowGet(e.target.checked)}
+                    label="Allow?"
+                    id="allowGet"
+                />
                 {allowGet && (
                     <>
-                        <CheckControl value={getRequireAuth} onChange={e => setGetRequireAuth(e.target.checked)} label="Require auth?" id="getAuth"/>
-                        <CheckControl value={getSingle} onChange={e => setGetSingle(e.target.checked)} label="Get single document?" id="getSingle"/>
+                        <CheckControl
+                            value={getRequireAuth}
+                            onChange={e => setGetRequireAuth(e.target.checked)}
+                            label="Require auth?"
+                            id="getAuth"
+                        />
+                        <CheckControl
+                            value={getSingle}
+                            onChange={e => setGetSingle(e.target.checked)}
+                            label="Get single document?"
+                            id="getSingle"
+                        />
                         {!getSingle && (
                             <>
-                                <CheckControl value={getPagination} onChange={e => setGetPagination(e.target.checked)} label="Enable pagination?" id="getPagination"/>
+                                <CheckControl
+                                    value={getPagination}
+                                    onChange={e => setGetPagination(e.target.checked)}
+                                    label="Enable pagination?"
+                                    id="getPagination"
+                                />
                                 {getPagination && (
-                                    <input type="number" value={paginationSkip} onChange={e => setPaginationSkip(+e.target.value)}/>
+                                    <input
+                                        type="number"
+                                        value={paginationSkip}
+                                        onChange={e => setPaginationSkip(+e.target.value)}
+                                    />
                                 )}
                             </>
                         )}
@@ -102,10 +139,20 @@ export default function EndpointGenerator({nameUppercase, fields}: { nameUpperca
                     </>
                 )}
                 <h3>Post</h3>
-                <CheckControl value={allowPost} onChange={e => setAllowPost(e.target.checked)} label="Allow?" id="allowPost"/>
+                <CheckControl
+                    value={allowPost}
+                    onChange={e => setAllowPost(e.target.checked)}
+                    label="Allow?"
+                    id="allowPost"
+                />
                 {allowPost && (
                     <>
-                        <CheckControl value={postRequireAuth} onChange={e => setPostRequireAuth(e.target.checked)} label="Require auth?" id="postAuth"/>
+                        <CheckControl
+                            value={postRequireAuth}
+                            onChange={e => setPostRequireAuth(e.target.checked)}
+                            label="Require auth?"
+                            id="postAuth"
+                        />
                         <EndpointParams
                             endpointFields={postFields}
                             setEndpointFields={setPostFields}
@@ -113,6 +160,13 @@ export default function EndpointGenerator({nameUppercase, fields}: { nameUpperca
                         />
                     </>
                 )}
+                <h3>Delete</h3>
+                <CheckControl
+                    value={allowDelete}
+                    onChange={e => setAllowDelete(e.target.checked)}
+                    label="Allow?"
+                    id="allowDelete"
+                />
             </FlexChild>
             <FlexChild>
                 <h2>Code output</h2>
@@ -124,7 +178,35 @@ import {NextApiRequest, NextApiResponse} from "next";
 ${((allowPost && postRequireAuth) || (allowGet && getRequireAuth)) ? "import {getSession} from \"next-auth/client\";" : ""}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    switch (req.method) {
+    switch (req.method) {    
+        ${allowGet ? `case "GET":
+            ${getRequireAuth ? authSnippet : ""}
+            if (!(${GetFieldsArr.filter(d => d.fieldType === "param").map(({fieldName}, i) => `${i === 0 ? "" : " || "}req.query.${fieldName}`).join("")})) {
+                return res.status(406);                        
+            }
+            
+            try {                
+                let conditions = {};
+
+                if (req.query.id) conditions["_id"] = req.query.id;
+                ${GetFieldsArr.filter(d => d.fieldType !== "default").map(({fieldName, fieldType}) => `if (req.query.${fieldName}) conditions["${fieldName}"] = ${getFieldParam(fieldType, "", fieldName, false)};
+                `).join("")}
+                         
+                await dbConnect();   
+            
+                const thisObject = await ${nameUppercase}Model.aggregate([
+                    {$match: conditions},
+                    ${getPagination ? `{$skip: (+req.query.page - 1) * ${paginationSkip}},
+                    {$limit: ${paginationSkip}},` : ""}
+                ]);
+                
+                if (!thisObject) return res.status(404);
+                
+                return res.status(200).json({data: thisObject});
+            } catch (e) {
+                return res.status(500).json({message: e});                        
+            }
+        ` : ""}    
         ${allowPost ? `case "POST":
             ${postRequireAuth ? authSnippet : ""}
             try {
@@ -159,35 +241,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             } catch (e) {
                 return res.status(500).json({message: e});            
             }
-        ` : ""}    
-        ${allowGet ? `case "GET":
-            ${getRequireAuth ? authSnippet : ""}
-            if (!(${GetFieldsArr.filter(d => d.fieldType === "param").map(({fieldName}, i) => `${i === 0 ? "" : " || "}req.query.${fieldName}`).join("")})) {
-                return res.status(406);                        
-            }
+        ` : ""}
+        ${allowDelete ? `case "DELETE":
+            ${authSnippet}
             
-            try {                
-                let conditions = {};
-
-                if (req.query.id) conditions["_id"] = req.query.id;
-                ${GetFieldsArr.filter(d => d.fieldType !== "default").map(({fieldName, fieldType}) => `if (req.query.${fieldName}) conditions["${fieldName}"] = ${getFieldParam(fieldType, "", fieldName, false)};
-                `).join("")}
-                         
-                await dbConnect();   
+            if (!req.body.id) return res.status(406);
             
-                const thisObject = await ${nameUppercase}Model.aggregate([
-                    {$match: conditions},
-                    ${getPagination ? `{$skip: (+req.query.page - 1) * ${paginationSkip}},
-                    {$limit: ${paginationSkip}},` : ""}
-                ]);
+            try {
+                await dbConnect();
+                               
+                const thisObject = await ${nameUppercase}Model.findById(req.body.id);
                 
                 if (!thisObject) return res.status(404);
+                if (thisObject.userId.toString() !== session.userId) return res.status(403);
                 
-                return res.status(200).json({data: thisObject});
+                await ${nameUppercase}Model.deleteOne({_id: req.body.id});
+                
+                return res.status(200).json({message: "Object deleted"});
             } catch (e) {
-                return res.status(500).json({message: e});                        
+                return res.status(500).json({message: e});
             }
-        ` : ""}    
+        ` : ""}
         default:
             return res.status(405);
     }
